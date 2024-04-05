ddd048aca7abd7611ae6b03d4f80fd98d803e7c2
MaherJamil <maherj@nycstudents.net>
2024-04-05 12:28:24 -0400

    h

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Beginning
 print("Hello Player!")
 User = input("Please enter a username: ")
 difficulty = input("Select a difficulty level (Baby mode/Average human/Super human): ")
@@ -6,3 +7,21 @@ print("Directions: This is an action-adventure game where you'll be able to trav
 print("The amount of damage you do will be based off of your answers (Try to be creative).")
 print("Interact with characters throughout the map to progress the story.")
 print("Have fun! (probably won't)")
+
+#Movement
+Direction = input("Forward/Backward/Lef/Right:  ")
+if Direction == "Left" or "left":
+    print(f"Moving {Direction}")
+elif Direction == "Right" or "right":
+    print(f"Moving {Direction}")
+elif Direction == "Forward" or "forward":
+    print(f"Moving {Direction}")
+elif Direction == "Backward" or "backward":
+    print(f"Moving {Direction}")
+
+#NPCs
+print("You've stumbled across [NPC]")
+npc = input("Press (E) to interact: ")
+if npc == "e":
+    print("[NPC]: Hello there! I'm tired and don't have enough energy to talk to you.")
+
