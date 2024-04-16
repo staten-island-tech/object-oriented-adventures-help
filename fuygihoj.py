2bbc329d0c1253e73dc87e6be58aa765afa00cc1
MaherJamil <maherj@nycstudents.net>
2024-04-16 12:28:54 -0400

    Update fuygihoj.py

---
+++ fuygihoj.py
@@ -0,0 +1,42 @@
+#Monsters
+player = []
+Php = 100
+Patk = 15
+player.append({"Php":Php, "Patk":Patk})
+
+Slime = []
+Shp = 30
+Satk = 5
+Slime.append({"Shp":Shp, "Satk":Satk})
+
+Blaze = []
+Bhp = 65
+Batk = 15
+Blaze.append({"Bhp":Bhp, "Batk":Batk})
+
+Helios = []
+Hhp = 60
+Hatk = 15
+Helios.append({"Hhp":Hhp, "Hatk":Hatk})
+
+Octo = []
+Ohp = 45
+Oatk = 12
+Octo.append({"Ohp":Ohp, "Oatk":Ohp})
+
+#Weapons
+Ray_gun = []
+Rdmg = 8
+Ray_gun.append({"Rdmg":Rdmg})
+
+Fire_sword = []
+Fdmg = 12
+Fire_sword.append({"Fdmg":Fdmg})
+
+Gernade = []
+Gdmg = 3
+Gernade.append({"Gdmg":Gdmg})
+
+The_power_of_the_sun = []
+Pdmg = 10000000000000
+The_power_of_the_sun.append({"pdmg":Pdmg})
