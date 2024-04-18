c79f404b9fd35d840b06c03b30da092852576424
MaherJamil <maherj@nycstudents.net>
2024-04-18 12:29:54 -0400

    Update Trash_game.py

--- Trash_game.py
+++ Trash_game.py
@@ -1,15 +1,36 @@
 import random
 
 #Random
-def Random():
-    random_number = random.randint(1,20)
-    if random_number == int(1,2,3,4,5,6,7,8,9,10):
-        print("Nothing")
-    if random_number == int(11,12,13,14,15,16,17):
-        print("Monster")
-    if random_number == int(18,19,20):
-        print("Chest")
-print("Welcome to Trash the game!")
+
+class Monster():
+    def Random():
+        random_number = random.randint(1,20)
+        if random_number in [1,2,3,4,5,6,7,8,9]:
+            print("Nothing")
+        if random_number in [10,11,12,13,14,15,16]:
+            print("Monster")
+        if random_number in [17,18,19,20]:
+            print("Chest")
+class Monster():
+        Slime = []
+        Mhp = 30
+        Matk = 5
+        Slime.append({"Shp":Mhp, "Satk":Matk})
+
+        Blaze = []
+        Mhp = 65
+        Matk = 15
+        Blaze.append({"Bhp":Mhp, "Batk":Matk})
+
+        Helios = []
+        Mhp = 60
+        Matk = 15
+        Helios.append({"Hhp":Mhp, "Hatk":Matk})
+
+        Octo = []
+        Mhp = 45
+        Matk = 12
+        Octo.append({"Ohp":Mhp, "Oatk":Mhp})
 
 User = input("Please enter a username: ")
 print(f"Welcome {User}")
@@ -20,16 +41,17 @@ if click == "":
     click = input("Use these weapons to fight monsters ")
     click = input("Lastly, have fun! (probably won't) ")
 
-Direction = input("Forward/Backward/Left/Right: ")
-if Direction == "Left" or "left":
-    print(f"Moving {Direction}")
-    Random()
-elif Direction == "Right" or "right":
-    print(f"Moving {Direction}")
-    Random()
-elif Direction == "Forward" or "forward":
-    print(f"Moving {Direction}")
-    Random()
-elif Direction == "Backward" or "backward":
-    print(f"Moving {Direction}")
-    Random()
+for i in range(24973932195):
+    Direction = input("Forward/Backward/Left/Right: ")
+    if Direction == "Left" or "left":
+        print(f"Moving {Direction}")
+        Random()
+    elif Direction == "Right" or "right":
+        print(f"Moving {Direction}")
+        Random()
+    elif Direction == "Forward" or "forward":
+        print(f"Moving {Direction}")
+        Random()
+    elif Direction == "Backward" or "backward":
+        print(f"Moving {Direction}")
+        Random()
