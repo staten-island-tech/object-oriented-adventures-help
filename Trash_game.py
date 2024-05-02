a4c116cfa364bd661dc7032491aa865637f81acc
MaherJamil <maherj@nycstudents.net>
2024-05-02 12:27:30 -0400

    Update Trash_game.py

--- Trash_game.py
+++ Trash_game.py
@@ -1,8 +1,10 @@
 import random
 
 #Random
+inventory = []
+
 def Random():
-    random_number = random.randint(1,25)
+    random_number = random.randint(1,23)
     if random_number in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
         print("Nothing")
     if random_number in [16,20]:
@@ -13,13 +15,27 @@ def Random():
         print(Helios)
     if random_number in [19,23]:
         print(Octo)
-    if random_number in [24,25]:
-        print("Chest")
 
 Slime = "[Slime] HP:(30) ATK:(5)"
 Blaze = "[Blaze] HP:(65) ATK:(15)"
 Helios = "[Helios] HP:(60) ATK:(15)"
 Octo = "[Octo] HP:(45} ATK:(12)"
+
+def loot():
+    loot = random.randint(1,40)
+    if loot in [1,2,3,4,5,6,7,8,9,10,11,12,13,14]:
+        print(Ray_gun)
+    if loot in [15,16,17,18,19,20,21,22]:
+        print(Fire_sword)
+    if loot in [23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39]:
+        print(Gernade)
+    if loot in [40]:
+        print(The_power_of_the_sun)
+
+Ray_gun = "[Ray Gun] DMG:(8)"
+Fire_sword = "[Fire Sword] DMG:(12)"
+Gernade = "[Gernade] DMG:(3)"
+The_power_of_the_sun = "[The Power of the Sun] DMG:(10000000000000)"
 
 User = input("Please enter a username: ")
 print(f"Welcome {User}")
