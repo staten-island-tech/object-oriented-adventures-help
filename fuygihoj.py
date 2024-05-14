4eca828f8fa41a7c94d496c7c7fbee19e9a0b674
ddeanh <deanh25@nycstudents.net>
2024-05-14 12:29:53 -0400

    garaga

--- fuygihoj.py
+++ fuygihoj.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-=======
-
-Monsters = [
-        {'slime':"HP:(30) ATK:(5)"},
-        {'blaze':"HP:(65) ATK:(15)"},
-        {'helios':"HP:(60) ATK:(15)"},
-        {'octo':"HP:(45) ATK:(12)"}]
-
->>>>>>> main
 #Monsters
 player = []
 Php = 100
