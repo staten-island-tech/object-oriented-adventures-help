4d35c51bb42b69a2df636471849de0644df3241e
MaherJamil <maherj@nycstudents.net>
2024-05-16 12:24:45 -0400

    Update suits.py

--- suits.py
+++ suits.py
@@ -1,19 +1,23 @@
-class suits():
-    def __init__(self, name: str, ability: int, dmg: int):
-        self.name = name
-        self.ability = ability
-        self.dmg = dmg
-
 Shockwave = "[Ability]:Energy Field || [Damage]:+3:"
 Magman = "[Ability]:Lava Spray || [Damage]:+2.5:"
 Hydro = "[Ability]:Water Pump || [Damage]:+3:"
 OcTitan = "[Ability]:SUN || [Damage]:+65:"
 
-Select = input("Select a suit (Shockwave/Magman/Hydro): ")
-if Select == "643":
+Select = input("Select a suit (Shockwave/Magman/Hydro): ").lower()
+if Select == "123":
     print("OcTitan Suit selected")
     print(OcTitan)
-elif Select == "Magman" or "magman":
-    print(f"{Select} Suit selected")
+elif Select == "magman":
+    print("Magman Suit selected")
+    print(Magman)
 elif Select == "hydro":
-    print(f"{Select} Suit selected")
+    print("Hydro Suit selected")
+    print(Hydro) 
+elif Select == "shockwave":
+    print(Shockwave)
+
+""" class suits():
+    def __init__(self, name: str, ability: int, dmg: int):
+        self.name = name
+        self.ability = ability
+        self.dmg = dmg """
