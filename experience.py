108d2e93d4d2d5e74fe07b6afad629d37ecb72d5
MaherJamil <maherj@nycstudents.net>
2024-05-21 12:26:54 -0400

    Update experience.py

---
+++ experience.py
@@ -0,0 +1,11 @@
+level = []
+Slime=0.2
+Blaze=0.3
+for i in range(24):
+    attk = input("Test: ")
+    if attk == "slime":
+        print(Slime)
+        level.append
+    if attk == "blaze":
+        print(Slime+Blaze)
+        level.append
