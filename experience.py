d176a701d5dbf344ff085a9da517a96c336bf080
MaherJamil <maherj@nycstudents.net>
2024-05-22 12:29:26 -0400

    s

--- experience.py
+++ experience.py
@@ -3,9 +3,12 @@ Slime=0.2
 Blaze=0.3
 for i in range(24):
     attk = input("Test: ")
-    if attk == "slime":
+    if attk == "s":
         print(Slime)
-        level.append
-    if attk == "blaze":
-        print(Slime+Blaze)
-        level.append
+        level.append(Slime)
+    elif attk == "b":
+        print(Blaze)
+        level.append(Blaze)
+    
+    for Slime in level:
+        print(Slime)
