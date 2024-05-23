7912e099a0f24a8740a43291d5814bbef36bd583
MaherJamil <maherj@nycstudents.net>
2024-05-23 12:29:25 -0400

    Update experience.py

--- experience.py
+++ experience.py
@@ -1,14 +1,27 @@
-level = []
+""" level = []
 Slime=0.2
 Blaze=0.3
 for i in range(24):
     attk = input("Test: ")
     if attk == "s":
-        print(Slime)
         level.append(Slime)
     elif attk == "b":
-        print(Blaze)
         level.append(Blaze)
     
     for Slime in level:
         print(Slime)
+ """
+level = []
+Slime=0.2
+Blaze=0.3
+for i in range(24): 
+    monster = (input("Test: "))
+    if monster == "slime":
+        level.append(Slime)
+    elif monster == "blaze":
+        level.append(Blaze)
+
+level.append(monster)
+print("XP:")
+for monster in level:
+    print(monster)
