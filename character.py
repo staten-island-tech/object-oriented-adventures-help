aff94f780bc3928acf08c4f10a910d7345b72beb
michaell741 <michaell741@nycstudents.net>
2024-06-07 12:18:44 -0400

    a

--- character.py
+++ character.py
@@ -1,4 +1,3 @@
-# character.py
 from weapons import *
 class Character:
     def __init__(self, name: str, health: int) -> None:
@@ -13,15 +12,16 @@ class Character:
         target.health = max(target.health, 0)
         if target.health == 0:
             
+        
             
-            
-            
-            
-            class Hero(Character):
-                def __init__(self,
-                    name: str,
-                    health: int) -> None:
-                        super().__init__(name=name, health=health)
+
+
+
+class Hero(Character):
+    def __init__(self,
+                 name: str,
+                 health: int) -> None:
+        super().__init__(name=name, health=health)
             
 
         self.default_weapon = self.weapon
@@ -48,27 +48,6 @@ class Enemy(Character):
         self.default_weapon = self.weapon
         
 
-    def equi(self, weapon) -> None:
+    def equip(self, weapon) -> None:
         self.weapon = weapon
-       
-
-Death = Enemy(name = "Death", health = 500 ) #Fire Sword
-Death.equi(Fire_sword)
-
-Archer = Enemy(name = "Archer", health = 30) #Recurve_Bow
-Archer.equi(Recurve_Bow)
-
-Dark_Mage = Enemy(name = "Dark Mage", health = 20) #Staff of the abyss
-Dark_Mage.equi(Staff_of_The_Abyss)
-
-Abyss_Knight = Enemy(name = "Abyss Knight", health= 50) #iron sword 
-Abyss_Knight.equi(Iron_sword)
-
-Brute =  Enemy(name = "Brute", health= 80) #iron sword
-Brute.equi(Iron_sword)
-
-Gorlock_the_Destroyer = Enemy(name = "Gorlock the Destroyer", health=200)#fists
-Gorlock_the_Destroyer.equi(fists)
-
-Skibidi_Rizzler = Enemy(name = "Skibidi Rizzler", health=300 )#grenade
-Skibidi_Rizzler.equi(Grenade)
+        print(f"{self.name} equipped a(n) {self.weapon.name}!")
